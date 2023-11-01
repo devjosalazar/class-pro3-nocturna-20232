24a1fe6647d8b0320b02697f5052ddc4ff454eb2
josalazar <53492638+devjosalazar@users.noreply.github.com>
2023-10-31 19:25:47 -0500

    hilos sincronizacion

---
+++ co.edu.uniquindio.programacion3/src/main/java/co/edu/uniquindio/programacion3/sockets/envioArchivos/MainCliente.java
@@ -0,0 +1,21 @@
+package co.edu.uniquindio.programacion3.sockets.envioArchivos;
+
+public class MainCliente {
+
+	public static void main(String[] args) {
+
+
+		Cliente miCliente = new Cliente(8081,"Libro1.xlsx");
+		
+		try {
+			miCliente.crearConexion();
+			miCliente.enviarArchivo();
+			
+		} catch (Exception e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
